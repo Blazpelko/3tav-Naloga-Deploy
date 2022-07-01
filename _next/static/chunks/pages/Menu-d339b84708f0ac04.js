f2a982ac66c3ef246243359321f14179a2a9d083
Blazpelko <61289252+Blazpelko@users.noreply.github.com>
2022-07-01 15:17:45 +0000

    Deploying to gh-pages from @ Blazpelko/3tav-Naloga-Deploy@14a9b7e6d1117a96d2c41e93765f4177ef497278 🚀

---
+++ _next/static/chunks/pages/Menu-d339b84708f0ac04.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[213],{7941:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/Menu",function(){return u(8052)}])}},function(n){n.O(0,[774,662,967,755,420,52,888,179],(function(){return _=7941,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
